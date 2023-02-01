ff3fe9a7bcc2171c7f08acb91fc6d03f0904870b
shoh4n <maffin.killer007@gmail.com>
2023-02-01 16:10:04 +0600

    another js file added for increase phone quantity and it's price

---
+++ js/phone.js
@@ -0,0 +1,40 @@
+// let's make a function for increase and decrease the phone quantity
+function updatePhoneQuantity (isIncrease){
+  // get the input field
+  const phoneInputField = document.getElementById('phone-input-field');
+  const phoneInputStr = phoneInputField.value;
+  const previousPhoneQuantity = parseInt(phoneInputStr);
+  
+  let newPhoneQuantity;
+  if( isIncrease === true ){
+    newPhoneQuantity = previousPhoneQuantity + 1;
+  }
+  else{
+    newPhoneQuantity = previousPhoneQuantity - 1;
+  }
+
+  phoneInputField.value = newPhoneQuantity;
+  return newPhoneQuantity;
+}
+
+
+// get the phone plus button and add event listener
+document.getElementById('phone-plus-btn').addEventListener('click', ()=>{
+  const newPhoneQuantity = updatePhoneQuantity( true );
+  // get the phone price
+  const totalPhonePrice = newPhoneQuantity * 1219;
+
+  const phonePriceElement = document.getElementById('phone-price');
+  phonePriceElement.innerText = totalPhonePrice;
+});
+
+// get the phone minus button and add event listener
+document.getElementById('phone-minus-btn').addEventListener('click', ()=>{
+  const newPhoneQuantity = updatePhoneQuantity( false );
+
+  // get the phone price
+  const totalPhonePrice = newPhoneQuantity * 1219;
+
+  const phonePriceElement = document.getElementById('phone-price');
+  phonePriceElement.innerText = totalPhonePrice;
+});
