69d52707735cc142f77f2df3c5b37c6f762b8627
shoh4n <maffin.killer007@gmail.com>
2023-02-01 13:19:17 +0600

    update everything using separate function

--- js/case.js
+++ js/case.js
@@ -1,22 +1,28 @@
-const caseBtnPlus = document.getElementById('case-btn-plus');
-caseBtnPlus.addEventListener('click', ()=>{
+function updateCaseQuantity ( isIncrease ) {
   const caseInputField = document.getElementById('case-input-field');
   const caseInputStr = caseInputField.value;
   const previousCaseQuantity = parseInt(caseInputStr);
-  const newCaseQuantity = previousCaseQuantity + 1;
+  let newCaseQuantity;
+
+  if ( isIncrease === true ) {
+    newCaseQuantity = previousCaseQuantity + 1;
+  }
+  else {
+    newCaseQuantity = previousCaseQuantity - 1;
+  }
 
   caseInputField.value = newCaseQuantity;
+  
+}
+
+
+document.getElementById('case-btn-plus').addEventListener('click', ()=>{
+  updateCaseQuantity( true );
 })
 
 
 // decrease button state 
-const caseBtnMinus = document.getElementById('case-btn-minus');
-caseBtnMinus.addEventListener('click', () =>{
-  const caseInputField = document.getElementById('case-input-field');
-  const caseInputFieldStr = caseInputField.value;
-  const previousCaseQuantity = parseInt(caseInputFieldStr);
-
-  const newCaseQuantity = previousCaseQuantity - 1;
-  caseInputField.value = newCaseQuantity;
+document.getElementById('case-btn-minus').addEventListener('click', () =>{
+  updateCaseQuantity( false );
   
 })
